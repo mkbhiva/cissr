b4a714706f87043a04656dd8649f5e3155bd34d6
Manoj Kumar <manojkr55@gmail.com>
2023-03-08 21:49:55 +0530

    Deleted health

--- application/modules/site_settings/controllers/Site_settings.php
+++ application/modules/site_settings/controllers/Site_settings.php
@@ -3,49 +3,57 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Site_settings extends MX_Controller {
+class Site_settings extends MX_Controller
+{
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
     }
-    
-    function _get_company() {
-        $company = "The Advocates League";
+
+    function _get_company()
+    {
+        $company = "SSR Foundation";
         return $company;
     }
-    
-    function _get_our_address() {
+
+    function _get_our_address()
+    {
         $our_address = " <br> <b> Correspondence Add: </b> C-72, New Ashok Nagar near MCD School, New Delhi-110096";
-       
+
         $our_address .= " <br> <!--- <b>Permanent Add:</b> New Area Hamidganj
         Daltonganj, Palamu, Jharkhand-822101 --->";
         return $our_address;
     }
-    
-    function _get_our_telnum() {
-        $telnum ="805-235-1466";
+
+    function _get_our_telnum()
+    {
+        $telnum = "805-235-1466";
         return $telnum;
     }
-    
-    function _get_our_email() {
+
+    function _get_our_email()
+    {
         $our_email = "info@theadvocatesleague.in";
         return $our_email;
     }
-    
-    function _get_post_segments(){
+
+    function _get_post_segments()
+    {
         $segments = "blog/article/";
         return $segments;
     }
-    
-    function _get_posts_segments(){
+
+    function _get_posts_segments()
+    {
         $segments = "blog/articles/";
         return $segments;
     }
-    
-    function _get_page_not_found_msg(){
+
+    function _get_page_not_found_msg()
+    {
         $msg = "<h1>This page not found at our website !!!</h1>";
-        $msg.="<p>Please check your link is not proper.</p>";
+        $msg .= "<p>Please check your link is not proper.</p>";
         return $msg;
     }
-
 }
